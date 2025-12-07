460d376dbe060651c9b81b9f6f2cd93a04b278e4
tomato252525 <87811573+tomato252525@users.noreply.github.com>
2025-12-07 19:18:47 +0900

    Fix import URL for Supabase client

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 (function () {
   // 設定値
